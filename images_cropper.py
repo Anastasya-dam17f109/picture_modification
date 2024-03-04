1b67c7c4eb11785333e39551c782cbf8efe2bac5
Anastasya <dost.bmstu99@gmail.com>
2024-03-04 12:36:21 +0300

    [new] new functionality for qtree

--- images_cropper.py
+++ images_cropper.py
@@ -7,7 +7,7 @@ filelist =     [
 ]
 
 psp_list=[
-"D:/image_data/__FR1_modified.jpg._PSP"
+"D:/_SAR_town/2_vers/__SAR_HH.TIF_crop.JPG_part.JPG._PSP"
 ]
 
 class image_modifier:
@@ -88,6 +88,45 @@ class image_modifier:
         picture.close()
         picture2.close()
 
+# суммирование изображений со специализацией цвета
+    def make_summ_predef(self, f1, f2,color):
+        picture = Image.open(f1)
+        picture2 = Image.open(f2)
+        width_p, height_p = picture.size
+        img_accumulated = Image.new('RGB', (width_p, height_p), color=0)
+        mass = np.array(picture, dtype="float32")
+        mass2 = np.array(picture2, dtype="float32")
+
+        for i in range(height_p):
+            for j in range(width_p):
+                img_accumulated.putpixel((j, i), (int(mass[i, j, 0]), int(mass[i, j, 1]), int(mass[i, j, 2])))
+
+        for i in range(height_p):
+            for j in range(width_p):
+                if int(mass2[i, j, 0]) !=0 or int(mass2[i, j, 1]) != 0 or int(mass2[i, j, 2]) != 0:
+                    img_accumulated.putpixel((j, i), (color[0], color[1], color[2]))
+        img_accumulated.save(f1 + "_sum.BMP")
+        img_accumulated.close()
+        picture.close()
+        picture2.close()
+
+    def make_inv_color(self, filename, old_color, new_color):
+        picture = Image.open(filename)
+        width_p, height_p = picture.size
+        img_accumulated = Image.new('RGB', (width_p, height_p), color=0)
+        mass = np.array(picture, dtype="float32")
+        print(mass.shape, width_p, height_p)
+        for i in range(height_p):
+            for j in range(width_p):
+
+                if int(mass[i, j, 0]) == old_color[0] and int(mass[i, j, 1]) == old_color[1] and int(mass[i, j, 2]) == old_color[2]:
+                    img_accumulated.putpixel((j, i), (new_color[0], new_color[1], new_color[2]))
+                else:
+                    img_accumulated.putpixel((j, i), (int(mass[i, j,0]), int(mass[i, j,1]), int(mass[i, j,2])))
+        img_accumulated.save(filename + "_inverted.BMP")
+        img_accumulated.close()
+        picture.close()
+#рисование блоков классов
     def draw_psp_list(self, psp_list, dir,img_base):
         for i in range(len(psp_list)):
             t_color = ['blue', 'red', 'yellow', 'green', 'violet']
@@ -142,7 +181,6 @@ class image_modifier:
         for i in range(height_p):
             for j in range(width_p):
 
-                # print(mass[i, j])
                 img_accumulated.putpixel((j, i), (int(mass[i, j, ch_num]), int(mass[i, j, ch_num]), int(mass[i, j, ch_num])))
         img_accumulated.save(filename + "_" + str(ch_num) + "channel.JPG")
         img_accumulated.close()
@@ -165,6 +203,24 @@ class image_modifier:
         img_accumulated.save(filename + "_" + str(ch_num1) + "_" + str(ch_num2) +  "_" + str(ch_num3) +"channel.JPG")
         img_accumulated.close()
         picture.close()
+
+    def make_substraction (self, f1, f2):
+        picture = Image.open(f1)
+        picture2 = Image.open(f2)
+        width_p, height_p = picture.size
+        img_accumulated = Image.new('RGB', (width_p, height_p), color=0)
+        mass = np.array(picture, dtype="float32")
+        mass2 = np.array(picture2, dtype="float32")
+
+        print(mass.shape, width_p, height_p)
+        for i in range(height_p):
+            for j in range(width_p):
+                if mass2[i,j,2] == 0:
+                    img_accumulated.putpixel((j, i), (int(mass[i, j, 0]), int(mass[i, j, 1]), int(mass[i, j, 2])))
+        img_accumulated.save(f1 + "_substract.BMP")
+        img_accumulated.close()
+        picture.close()
+        picture2.close()
 
     def draw_psp_list_contours(self, psp_list, dir,img_base):
         temple_file = Image.open(img_base)
@@ -232,6 +288,11 @@ mod.set_filelist(filelist)
 #mod.make_square("E:/_SAR_town/__SAR_HH.TIF_crop.JPG_part.JPG")
 #mod.make_log("E:/_SAR_town/__SAR_HH.TIF_crop.JPG_part.JPG")
 #mod.make_summ(filelist[2], filelist[5])
-mod.draw_psp_list(psp_list, "D:/image_data/","D:/image_data/__FR1_modified.jpg")
+#mod.draw_psp_list(psp_list, "D:/_SAR_town/2_vers/", "D:/_SAR_town/2_vers/__SAR_HH.TIF_crop.JPG_part.JPG")
 #mod.make_one_channel("C:/Users/ADostovalova/Downloads/q04indrex0401x2_t04_oceanmouth.jpeg")
 #mod.make_one_channel_as_summ("C:/Users/ADostovalova/Downloads/q04indrex0401x2_t04_oceanmouth.jpeg")
+#mod.make_summ_predef("D:/_SAR_town/new_mask_class_lbl_0.BMP", "D:/_SAR_town/2_vers/__SUMM_RGB.TIF-JSON.BMP_crop.JPG_part.JPG_1c.bmp", [0,128,0])
+#mod.make_summ_predef("D:/_SAR_town/new_mask_class_lbl_0.BMP_sum.BMP","D:/_SAR_town/2_vers/new_mask_class_0.BMP_part.bmp", [0,0,255])
+
+#mod.make_inv_color("D:/_SAR_town/new_mask_class_lbl_0.BMP_sum.BMP_sum_red.BMP_inverted.BMP",[238,130,238], [255,255,0])
+mod.make_substraction ("D:/_SAR_town/new_mask_class_lbl_0.BMP_sum.BMP_sum_red.BMP_inverted.BMP_inverted.BMP", "D:/_SAR_town/2_vers/new_mask_class_lbl_0.BMP")
